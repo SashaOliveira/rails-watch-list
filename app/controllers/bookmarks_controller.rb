c2fcb9ecb7ccb44ca52e20c5079cae5bb89129f2
Aleksandra Stoliarova <stoliarova.alek@gmail.com>
2024-08-15 17:55:23 +0200

    Simple form done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,20 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmarks = Bookmark.all
-  end
-
   def new
+    @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
     @bookmark.save
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
